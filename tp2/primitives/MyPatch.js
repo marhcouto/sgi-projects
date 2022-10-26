64f7d4f50eed64c14593cd94674b055c5f3afcf1
Marcelo Couto <up201906086@fe.up.pt>
2022-10-26 10:22:18 +0100

    fix(MyPatch): fixed MyPatch implementation

--- tp2/primitives/MyPatch.js
+++ tp2/primitives/MyPatch.js
@@ -1,17 +1,20 @@
 import { CGFnurbsObject, CGFnurbsSurface, CGFobject } from '../../lib/CGF.js';
 
 export class MyPatch extends CGFobject {
-    constructor(degU, partsU, degV, partsV, vertices) {
+    constructor(scene, degU, partsU, degV, partsV, vertices) {
+        super(scene);
+        this.scene = scene;
         this.degU = degU;
         this.degV = degV;
         this.partsU = partsU;
         this.partsV = partsV;
-        this.vertsU = vertices;
+        this.vertices = vertices;
+        this.initBuffers();
     }
 
     initBuffers() {
-        let nurbsSurface = CGFnurbsSurface(this.degU, this.degV, this.vertices);
-        this.nurbsObject = CGFnurbsObject(this.partsU, this.partsV, nurbsSurface);
+        let nurbsSurface = new CGFnurbsSurface(this.degU, this.degV, this.vertices);
+        this.nurbsObject = new CGFnurbsObject(this.scene, this.partsU, this.partsV, nurbsSurface);
     }
 
     display() {
